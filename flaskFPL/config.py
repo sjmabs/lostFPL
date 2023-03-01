4e7569910d30c0157972e1a5f673852d6d2ce932
Shaun Mabin <70712946+sjmabs@users.noreply.github.com>
2023-03-01 17:54:33 +0000

    Delete flaskFPL directory

--- flaskFPL/config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    SECRET_KEY = 'putakeyhere'
-
-# this is so it works out for people running from git but if you want to put it on a server you need to hide this key
-# and generate a new one
